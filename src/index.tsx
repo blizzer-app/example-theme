9d47b0e1abf25c5a2f2c36e0ac7aecfc9b4a2846
Daniel Hitzel <der-daniel@outlook.com>
2022-07-01 16:18:08 +0200

    wip

--- src/index.tsx
+++
@@ -1,7 +0,0 @@
-import * as React from 'react'
-import { createRoot } from 'react-dom/client'
-
-const Greet = () => <h1>Hello, world!</h1>
-
-const root = createRoot(document.getElementById('root'))
-root.render(<Greet />)
